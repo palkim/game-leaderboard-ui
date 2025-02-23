b37bf869595f639ccfae0ea7f5c5fae3d238d52a
petek.akcam <petek.akcam@kuartis.com>
2025-02-23 19:39:20 +0300

    add earnings form

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,35 @@ export default function Home() {
     toast.success(`Player created successfully: ${name}`);
   };
 
+  const handleSubmitEarn = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const playerId = formData.get("playerId") as string;
+    const amount = formData.get("amount") as string;
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    if (!apiUrl) {
+      console.error("API URL is not defined");
+      toast.error("API URL is not defined");
+      return;
+    }
+    const response = await fetch(`${apiUrl}/leaderboard/earn`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        playerId,
+        amount,
+      }),
+    });
+    if (!response.ok) {
+      const errorData = await response.json();
+      toast.error(errorData.error);
+      return;
+    }
+    toast.success(`Player with ID ${playerId} earned ${amount} money`);
+  };
+
   return (
     <div className="min-h-screen p-8 font-[family-name:var(--font-geist-sans)] w-full">
       <main className="flex justify-between items-start w-full">
@@ -79,7 +108,33 @@ export default function Home() {
             View Leaderboard
           </a>
         </div>
-        <div className="flex-1 items-center justify-center h-full"></div>
+        <div className="flex-1 items-center justify-center h-full">
+          <form onSubmit={handleSubmitEarn} className="flex flex-col gap-4 w-1/2 p-8">
+            <label htmlFor="playerId" className="text-white">
+              Player ID:
+            </label>
+            <input
+              type="text"
+              id="playerId"
+              name="playerId"
+              required
+              className="bg-[#241e3e] border border-[#332a65] rounded-md text-white p-2"
+            />
+            <label htmlFor="amount" className="text-white">
+              Amount:
+            </label>
+            <input
+              type="number"
+              id="amount"
+              name="amount"
+              required
+              className="bg-[#241e3e] border border-[#332a65] rounded-md text-white p-2"
+            />
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Earn Money
+            </button>
+          </form>
+        </div>
       </main>
     </div>
   );
